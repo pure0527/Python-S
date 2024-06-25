ce40fddaa2ebfc8c6a175b0eea992909b9db65ba
pure0527 <69231484+pure0527@users.noreply.github.com>
2024-06-25 10:28:42 -0500

    Update codition on if

--- example.java
+++ example.java
@@ -9,11 +9,11 @@ public class Main {
         for(int i=0; i<num; i++){
             System.out.println(patron);
             patron = patron + "#";
-            /*if(i==num-1){
-                for (i=num; i>0; i--){
+            if(i==num-1){
+                for (i=num; i>=0; i--){ //cambio de i>0 a i>=0
                     System.out.println(patron);
                 }
-            }*/
+            }
         }
     }
 }
