ccdd7fcd9fc11fc8b248869a84d4683dc7f5a380
Slap <advervins@gmail.com>
2024-11-08 13:33:43 -0500

    Second commit

--- Flower.py
+++ Flower.py
@@ -1,7 +1,10 @@
+#import tkinter.messagebox
 from turtle import *
 import colorsys
+import tkinter as tk
+from tkinter import messagebox
 
-speed(0)
+speed(-1)
 bgcolor("black")
 h=0
 
@@ -11,10 +14,15 @@ for i in range(16):
         color(c)
         h+=0.005
         rt(90)
-        circle(150-j*6,90)
+        circle(170-j*6,90)
         lt(90)
-        circle(150-j*6,90)
+        circle(170-j*6,90)
         rt(180)
     circle(40,24)
 
+root = tk.Tk()
+root.withdraw()
+
+messagebox.showinfo('Titulo','Mensaje')
+root.destroy()
 done()
